2624e4da5de62a51c97c2a8f1146a86e54197a20
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-24 18:53:32 +0300

    Changing the DOM Elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+// has a 'newHeader' variable that points to an <h1> node
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Joy is the champion!";
